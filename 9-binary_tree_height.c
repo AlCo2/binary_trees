e4f06aaa6a959d6d7d50688c92c2aa8bc3e7dffe
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-01 01:36:43 +0100

    fix the logic for the task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,12 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (tree == NULL)
-		return (height);
-	if (tree->left)
-		height += 1;
-	if (tree->right)
-		height += 1;
-	return (height);
+		return (0);
+	height_l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	height_r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	return ((height_l > height_r) ? height_l : height_r);
 }
